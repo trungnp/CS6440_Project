986d1ccaa8247c8dc2c2a48c980e29059240662a
Trung Nguyen <trung.np28@gmail.com>
2025-03-26 22:11:16 -0400

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,237 @@
+import pandas as pd
+import streamlit as st
+from streamlit_calendar import calendar
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+
+client = st.session_state["client"]
+
+
+def is_over_18(dob: datetime):
+    # Current date: March 19, 2025
+    current_date = datetime.now()
+
+    try:
+        # Parse the DOB string (assuming format YYYY-MM-DD)
+        # dob = datetime.strptime(dob_str, '%Y-%m-%d')
+
+        # Calculate age
+        age = current_date.year - dob.year
+
+        # Adjust age if birthday hasn't occurred this year
+        if (current_date.month, current_date.day) < (dob.month, dob.day):
+            age -= 1
+
+        # Check if under 18
+        return age >= 18
+
+    except ValueError:
+        return "Invalid date format. Please use YYYY-MM-DD"
+
+
+@st.cache_data(ttl=600)
+def search_patient(count=10, under_age=18, id=None, first_name=None, last_name=None, dob: datetime = None):
+    """
+    Fetch all patients under 5 years old using FHIR search
+    """
+    # if under_age > 18:
+    #     st.error("Only patients under 18 years old are supported.")
+    #     return
+    #
+    # if dob and is_over_18(dob):
+    #     st.error("Patient is over 18 years old.")
+    #     return
+
+    if id is None and first_name is None and last_name is None and dob is None:
+        params = {
+            "birthdate": f"gt{(datetime.now() - timedelta(days=under_age * 365)).strftime('%Y-%m-%d')}"
+        }
+    elif id is not None:
+        params = {
+            "_id": id
+        }
+    elif first_name is not None and last_name is not None and dob is not None:
+        params = {
+            "given": first_name,
+            "family": last_name,
+            "birthdate": dob,
+        }
+    else:
+        st.error("Invalid parameters. Please provide either ID or First Name, Last Name, and DOB.")
+
+    patients = client.resources('Patient').search(**params).limit(count).fetch()
+    # patients = client.resources('Patient').search(birthdate=f'gt{five_years_ago}').limit(count).fetch()
+    patients_list = [patient.serialize() for patient in patients]
+
+    return patients_list
+
+
+def display_calendar(events):
+    colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF", "#33FFF5", "#FF8C33", "#8CFF33", "#338CFF", "#FF338C"]
+    date_to_events = {}
+
+    for event in events:
+        event_date = event["start"].split("T")[0]
+        if event_date not in date_to_events:
+            date_to_events[event_date] = []
+        date_to_events[event_date].append(event)
+
+    for event_date, events_on_date in date_to_events.items():
+        for i, event in enumerate(events_on_date):
+            event["color"] = colors[i % len(colors)]
+            # start_date = event["start"].split("T")[0]
+            # end_date = event["end"].split("T")[0]
+            # if start_date != end_date:
+            #     event["classNames"] = ["event-range", "event-start", "event-end"]
+            # event["startClassNames"] = ["event-start"]
+            # event["endClassNames"] = ["event-end"]
+
+    options = {
+        # "editable": True,
+        "selectable": True,
+        # "headerToolbar": {
+        #     # "left": "prev,next",
+        #     # "center": "title",
+        #     # "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+        # },
+        # "slotMinTime": "06:00:00",
+        # "slotMaxTime": "18:00:00",
+        "initialView": "multiMonthYear",
+        # "dayMaxEventRows": 3,
+        # "size": "1000x300",
+        "height": "550px",
+        # "months": 4,
+    }
+
+    custom_css = """
+        .fc-event-past {
+            opacity: 0.8;
+        }
+        .fc-event-time {
+            font-style: italic;
+        }
+        .fc-event-title {
+            font-weight: 700;
+        }
+        .fc-toolbar-title {
+            font-size: 2rem;
+        }
+    """
+
+    cld = calendar(
+        events=events,
+        options=options,
+        custom_css=custom_css,
+        key='calendar',  # Assign a widget key to prevent state loss
+    )
+
+    st.write(cld)
+
+
+def send_email(to_email, vaccine_name, date_to_get, dose):
+    brevo_info = st.secrets["email"]
+    SENDER = brevo_info["SENDER"]
+    PWD = brevo_info["PWD"]
+    SENDER_EMAIL = brevo_info["SENDER_EMAIL"]
+    SENDER_PASSWORD = brevo_info["SENDER_PASSWORD"]
+    SMTP_SERVER = brevo_info["SMTP_SERVER"]
+    SMTP_PORT = brevo_info["SMTP_PORT"]
+    subject = f"Upcoming Immunization Reminder: {vaccine_name}"
+    body = f"""
+    Dear User,
+
+    This is a reminder for your upcoming immunization:
+    - Vaccine: {vaccine_name}
+    - Date: {date_to_get}
+    - Dose: {dose}
+
+    Please schedule your appointment if you haven’t already.
+    """
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = SENDER
+    msg["To"] = to_email
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(SENDER, PWD)
+            server.send_message(msg)
+        return True
+    except Exception as e:
+        return str(e)
+
+
+def write_schedule_to_csv(df):
+    try:
+        current_schedule = pd.read_csv("schedule.csv", header=0)
+    except pd.errors.EmptyDataError:
+        df.to_csv("schedule.csv", index=False)
+        return
+
+    if current_schedule.shape[0] > 0:
+        # new_patient = tuple(df[['patient_id', 'email']].values[0])
+        # patient_found = False
+        # for _, row in current_schedule[['patient_id', 'email']].drop_duplicates().iterrows():
+        #     if (row['patient_id'], row['email']) == new_patient:
+        #         patient_found = True
+        #         break
+        #
+        # if patient_found:
+        #     current_schedule = current_schedule[
+        #         (current_schedule['patient_id'] != new_patient[0]) |
+        #         (current_schedule['email'] != new_patient[1])
+        #         ]
+
+        new_email = df['email'].values[0]
+        if new_email in current_schedule['email'].values:
+            current_schedule = current_schedule[current_schedule['email'] != new_email]
+        df = pd.concat([current_schedule, df], ignore_index=True)
+    df.to_csv("schedule.csv", index=False, header=True)
+
+
+# Run every 3 hours
+@st.fragment(run_every=10800)
+def check_and_send_email():
+    df = pd.read_csv("schedule.csv", header=0)
+    df = df[df["is_sent"] == False]
+    current_date = datetime.now().date()
+    for idx, entry in df.iterrows():
+        dose = entry["dose"]
+        vaccine_name = entry["vaccine"]
+        notification_date = entry["date_to_send"]
+        notification_date = datetime.strptime(notification_date, "%Y/%m/%d").date()
+        email = entry["email"]
+
+        # st.write(f"- **{vaccine_name}**: {date_to_get} (Dose: {dose})")
+        # st.write(f"  Notification date: {notification_date}")
+
+        # Check if today is the notification date
+        if current_date >= notification_date and not entry["is_sent"]:
+            result = send_email(email, vaccine_name, entry["date_to_send"], dose)
+            if result is True:
+                df.loc[idx, "is_sent"] = True
+                df.to_csv("schedule.csv", index=False, header=True)
+                st.success(f"Reminder email sent to {email} for {vaccine_name}!")
+                st.stop()
+            else:
+                st.error(f"Failed to send email: {result}")
+        #     if result is True:
+        #         st.success(f"Reminder email sent to {email} for {vaccine_name}!")
+        #     else:
+        #         st.error(f"Failed to send email: {result}")
+        # elif current_date < notification_date:
+        #     st.write(f"  Reminder will be available on {notification_date}")
+        # else:
+        #     st.write(f"  Notification date has passed")
+
+
+def read_schedule_from_csv():
+    try:
+        return pd.read_csv("schedule.csv", header=0)
+    except pd.errors.EmptyDataError:
+        return None
+    except FileNotFoundError:
+        return None
