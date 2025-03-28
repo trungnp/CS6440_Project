bb3d551d2cd0559d358b06310da777ea3c77d59b
Trung Nguyen <trung.np28@gmail.com>
2025-03-28 10:07:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import streamlit as st
 from fhirpy import SyncFHIRClient
 
 st.set_page_config(page_title="CDC Immunization Schedule Reminder", layout="wide")
-# FHIR server setup
-# FHIR_SERVER_URL = "https://hapi.fhir.org/baseR4"  # Local HAPI instance
-# client = SyncFHIRClient('https://hapi.fhir.org/baseR4')
-# if 'client' not in st.session_state:
-#     st.session_state.client = client
+
+st.title("CDC Immunization Schedule Reminder, Please Choose a Role In Sidebar to Continue")
