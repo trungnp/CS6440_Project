2069efd59e8b75013c47b729b4a6f140d9257c70
Trung Nguyen <trung.np28@gmail.com>
2025-03-28 10:08:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from fhirpy import SyncFHIRClient
 
 st.set_page_config(page_title="CDC Immunization Schedule Reminder", layout="wide")
 
-st.title("CDC Immunization Schedule Reminder, Please Choose a Role In Sidebar to Continue")
+st.title("CDC Immunization Schedule Reminder")
+st.markdown("*Please Choose a Role In Sidebar to Continue*")
